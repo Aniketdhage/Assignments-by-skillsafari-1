4c4cf68a47d2c35ca9c548eab305c613c74b89f7
Aniket Dhage <68240795+Aniketdhage@users.noreply.github.com>
2021-08-21 13:37:26 +0530

    aded

--- javascript/1-operator,branching,loops.js
+++ javascript/1-operator,branching,loops.js
@@ -1,5 +1,56 @@
 // Write a program to add 5 numbers. The value of numbers are num1=5, num2=13, num3=7, num4=21 and num5=48.
 
-let num1=5,num2=13, num3=7, num4=21, num5=48;
-let add=num1+num2+num3+num4+num5;
-console.log(`The addition of five numbers is ${add}`);
+// let num1=5,num2=13, num3=7, num4=21, num5=48;
+// let add=num1+num2+num3+num4+num5;
+// console.log(`The addition of five numbers is ${add}`);
+
+// Write a program to take a number input from user and determine whether the number is odd or even.
+
+// let number =+prompt("Enter a number : ");
+
+// if(number%2===0){
+//     console.log(`The ${number} is even.`);
+// } else {
+//     console.log(`The ${number} is odd.`);
+// }
+
+// Write a program to find the maximum and minimum out of two given numbers. The numbers are num1=129 and num2=251.
+
+// let num1=129, num2=251;
+
+// if (num1>num2){
+//     console.log(`${num1} is greater than ${num2}`);
+// } else {
+//     console.log(`${num2} is greater than ${num1}`);
+// }
+
+// Write a program to find the maximum out of three given numbers. The numbers are num1=8, num2=23 and num3=17.
+
+// let  num1=8, num2=23, num3=17;
+
+// if (num1>num2 && num1>num3){
+//     console.log(`${num1} is greater than ${num2} and ${num3}`);
+// }
+// else if (num2>num1 && num2>num3) {
+//     console.log(`${num2} is greater than ${num1} and ${num3}`);
+// }
+// else {
+//     console.log(`${num3} is greater than ${num1} and ${num2}`);
+// }
+
+// Write a program to find the minimum out of three given numbers. The numbers are num1=35, num2=29 and num3=46.
+
+// let  num1=35, num2=29, num3=46;
+
+// if (num1<num2 && num1<num3){
+//     console.log(`${num1} is smaller than ${num2} and ${num3}`);
+// }
+// else if (num2<num1 && num2<num3) {
+//     console.log(`${num2} is smaller than ${num1} and ${num3}`);
+// }
+// else {
+//     console.log(`${num3} is smaller than ${num1} and ${num2}`);
+// }
+
+// Write program to take a month as an input from the user and find out whether the month has 31 days or not.
+
